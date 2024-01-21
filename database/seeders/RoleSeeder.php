5e8ef97b92fc4c31be2cd588bbc0cbfb6d33c9f9
eaglefox82 <fox82b@gmail.com>
2024-01-21 14:50:15 +1100

    Updated Roles
    Added Roles Framework
    #9

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -17,5 +17,11 @@ class RoleSeeder extends Seeder
         //
         $adminRole = Role::create(['name' => 'admin']);
         $userRole = Role::create(['name' => 'user']);
+        $userRole = Role::create(['name' => 'OC']);
+        $userRole = Role::create(['name' => 'Adjutant']);
+        $userRole = Role::create(['name' => '2nd Officer']);
+        $userRole = Role::create(['name' => 'Warrent Officer']);
+        $userRole = Role::create(['name' => 'NCO']);
+    }
     }
 }
